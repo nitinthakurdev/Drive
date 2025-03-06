fe0d62bc9aed140923ac1bc29919a347cbf6a097
nitinthakurdev <radheradhe9870023@gmail.com>
2025-03-06 16:44:44 +0530

    some changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,34 @@
-import Header from '@/components/Header';
-import React, {  FC, ReactNode } from 'react';
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import React, { FC, ReactNode } from 'react';
 
-const RootLayout:FC<{children:ReactNode}> = ({children}) => {
+const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <div>
-        <Header/>
-      {children}
+
+      <div>
+        <header className="flex justify-between items-center p-4 gap-4 h-14  shadow-md">
+          <div>
+            <Image src={"/images/mainIcons.png"} alt='logo' width={30} height={30} />
+          </div>
+          <div>
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+
+        </header>
+        <div className='w-full flex '>
+          <aside className='w-[200px] bg-gray-300' >
+            hello
+          </aside>
+          {children}
+        </div>
+      </div>
     </div>
   );
 };
